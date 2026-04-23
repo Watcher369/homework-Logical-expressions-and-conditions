d88f1af5e6454cda3bd71f8675c373b278a42faa
alyat <black1shooter@gmail.com>
2026-04-23 21:06:48 +0300

    Добавлено домашнее задание

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+age = 20
+is_citizen = True
+is_disqualified = False
+
+if age >= 18 and is_citizen and not is_disqualified:
+    print("Человек может голосовать")
+else:
+    print("Человек не может голосовать")
