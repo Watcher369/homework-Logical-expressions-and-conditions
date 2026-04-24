7d8237b42df77f98f2ded6ce7b52f46b46682707
alyat <black1shooter@gmail.com>
2026-04-24 18:22:50 +0300

    Обновлено домашнее задание

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 
 
-age = 20
-is_citizen = True
-is_disqualified = False
+def get_user_data():
+    age = int(input("Введите ваш возраст: "))
+    citizen = input("Вы гражданин страны? (да/нет): ").lower()
+    restriction = input("Есть ли у вас запрет на голосование? (да/нет): ").lower()
+    return age, citizen, restriction
 
-if age >= 18 and is_citizen and not is_disqualified:
-    print("Человек может голосовать")
-else:
-    print("Человек не может голосовать")
+
+def can_vote(age, citizen, restriction):
+    return age >= 18 and citizen == "да" and restriction == "нет"
+
+
+def show_result(result):
+    if result:
+        print("Вы можете голосовать.")
+    else:
+        print("Вы не можете голосовать.")
+
+
+age, citizen, restriction = get_user_data()
+result = can_vote(age, citizen, restriction)
+show_result(result)
